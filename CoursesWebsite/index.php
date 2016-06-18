b906f9de6858b8e3274fd60c632e3b3d4f305506
kingsman142 <kingsman142@yahoo.com>
2016-06-17 22:27:25 -0400

    added script.js for functionality, and slightly modified site.css

--- CoursesWebsite/index.php
+++ CoursesWebsite/index.php
@@ -2,17 +2,30 @@
 <html>
 <head>
     <link rel="stylesheet" type="text/css" href="Site.css" />
+    <script src="script.js" />
     <title></title>
 </head>
 <body src="Site.css">
     <div class="column">
-        Course
+        Course Title
+    </div>
+    <div class="column">
+        Course Description
     </div>
     <div class="column">
         Department
     </div>
     <div class="column">
         Instructor
+    </div>
+    <div class="column">
+        Currently Enrolled
+    </div>
+    <div class="column">
+        Capacity
+    </div>
+    <div class="column">
+        Status
     </div>
     <?php
     $servername = "DESKTOP-05D9CV7";
