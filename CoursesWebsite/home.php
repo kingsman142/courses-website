165287ed7e011f914fdcf5d4de0d94c10e2e8764
kingsman142 <kingsman142@yahoo.com>
2016-07-23 17:25:39 -0400

    modified some spacing and removed some commented code

--- CoursesWebsite/home.php
+++ CoursesWebsite/home.php
@@ -9,30 +9,28 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body src="Site.css">
-        <!--<div id="main">-->
-            <center>
-                <div class="title">
-                    <div id="jobTitle">Job<div id="exploreTitle">Explore</div></div>
-                </div>
-            </center>
-
-            <div id="menuWrapper">
-                <div class="menuItem"><img class="icon" src="images/searchJob.png" /><input type="text" class="searchForm" onkeydown="if(enterKey(event)){ searchForJob() }" placeholder="Search by job" /></div>
-                <div class="menuItem"><img class="icon" src="images/searchSkill.png" /><input type="text" class="searchForm" onkeydown="if(enterKey(event)){ searchForSkill() }" placeholder="Search by skill" /></div>
+        <center>
+            <div class="title">
+                <div id="jobTitle">Job<div id="exploreTitle">Explore</div></div>
             </div>
+        </center>
 
-            <div id="formsWrapper">
-                <div class="formText job">Occupation</div>
-                <div class="formText skill">Skill</div><br/>
-                <input class="form job" placeholder="Occupation"/>
-                <input class="form skill" placeholder="Skill"/><br/>
-                <button type="submit" id="updateDatabase" onclick="updateDatabase()">Help others find a career path!</button>
-            </div>
+        <div id="menuWrapper">
+            <div class="menuItem"><img class="icon" src="images/searchJob.png" /><input type="text" class="searchForm" onkeydown="if(enterKey(event)){ searchForJob() }" placeholder="Search by job" /></div>
+            <div class="menuItem"><img class="icon" src="images/searchSkill.png" /><input type="text" class="searchForm" onkeydown="if(enterKey(event)){ searchForSkill() }" placeholder="Search by skill" /></div>
+        </div>
 
-            <div id="tableWrapper">
-                <table id="jobStatsTable"><caption align="center">Top Five Jobs</caption><tbody></tbody></table>
-                <table id="skillStatsTable"><caption align="center">Top Five Skills</caption><tbody></tbody></table>
-            </div>
-        <!--</div>-->
+        <div id="formsWrapper">
+            <div class="formText job">Occupation</div>
+            <div class="formText skill">Skill</div><br/>
+            <input class="form job" placeholder="Occupation"/>
+            <input class="form skill" placeholder="Skill"/><br/>
+            <button type="submit" id="updateDatabase" onclick="updateDatabase()">Help others find a career path!</button>
+        </div>
+
+        <div id="tableWrapper">
+            <table id="jobStatsTable"><caption align="center">Top Five Jobs</caption><tbody></tbody></table>
+            <table id="skillStatsTable"><caption align="center">Top Five Skills</caption><tbody></tbody></table>
+        </div>
     </body>
 </html>
--- CoursesWebsite/index.php
+++ CoursesWebsite/index.php
@@ -8,57 +8,57 @@
         <title>Job Explore</title>
     </head>
     <body src="Site.css">
-            <center>
-                <div id="main">
-                    <div class="title">
-                        <?php
-                        $jobOrSkill = "";
-                        $data = "";
-                        if($_GET['job']){
-                            $jobOrSkill = "Job";
-                            $data = $_GET['job'];
+        <center>
+            <div id="main">
+                <div class="title">
+                    <?php
+                    $jobOrSkill = "";
+                    $data = "";
+                    if($_GET['job']){
+                        $jobOrSkill = "Job";
+                        $data = $_GET['job'];
+                    } else{
+                        $jobOrSkill ="Skill";
+                        $data = $_GET['skill'];
+                    }
+
+                    echo '<div id="indexTitle">' . $jobOrSkill . ': <b>' . $data . '</b></div>';
+                    ?>
+                </div>
+
+                <div class="contentCard green" id="associatedTags">
+                    <div class="cardTitle">
+                    <?php
+                        $tagText = "";
+                        if($jobOrSkill == "Job"){
+                            $tagText = "skills";
+                        } else if($jobOrSkill == "Skill"){
+                            $tagText = "jobs";
                         } else{
-                            $jobOrSkill ="Skill";
-                            $data = $_GET['skill'];
+                            $tagText = "tags";
                         }
 
-                        echo '<div id="indexTitle">' . $jobOrSkill . ': <b>' . $data . '</b></div>';
-                        ?>
+                        echo "Most popular tagged " . $tagText;
+                    ?>
                     </div>
-
-                    <div class="contentCard green" id="associatedTags">
-                        <div class="cardTitle">
-                        <?php
-                            $tagText = "";
-                            if($jobOrSkill == "Job"){
-                                $tagText = "skills";
-                            } else if($jobOrSkill == "Skill"){
-                                $tagText = "jobs";
-                            } else{
-                                $tagText = "tags";
-                            }
-
-                            echo "Most popular tagged " . $tagText;
-                        ?>
-                        </div>
-                    </div>
-
-                    <div class="contentCard purple" id="fillerCard">
-                    </div>
-
-                    <a class="twitter-timeline" href="https://twitter.com/CAREEREALISM" data-width="31%" data-height="858px" data-chrome="noscrollbar noheader nofooter noborders" data-theme="light">Tweets by LaddersInc</a>
-                    <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
                 </div>
-            </center>
 
-            <div id="menuWrapper">
-                <div class="menuItem"><img class="icon" src="images/searchJob.png" /><input type="text" class="searchForm" onkeydown="if(enterKey(event)){ searchForJob() }" placeholder="Search by job" /></div>
-                <div class="menuItem"><img class="icon" src="images/searchSkill.png" /><input type="text" class="searchForm" onkeydown="if(enterKey(event)){ searchForSkill() }" placeholder="Search by skill" /></div>
-                <div class="menuItem"><img class="icon" src="images/home.png" /><a type="submit" id="homeLink" href="home.php">Home</a></div>
+                <div class="contentCard purple" id="fillerCard">
+                </div>
+
+                <a class="twitter-timeline" href="https://twitter.com/CAREEREALISM" data-width="31%" data-height="858px" data-chrome="noscrollbar noheader nofooter noborders" data-theme="light">Tweets by LaddersInc</a>
+                <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
             </div>
+        </center>
 
-            <div id="tableWrapper">
+        <div id="menuWrapper">
+            <div class="menuItem"><img class="icon" src="images/searchJob.png" /><input type="text" class="searchForm" onkeydown="if(enterKey(event)){ searchForJob() }" placeholder="Search by job" /></div>
+            <div class="menuItem"><img class="icon" src="images/searchSkill.png" /><input type="text" class="searchForm" onkeydown="if(enterKey(event)){ searchForSkill() }" placeholder="Search by skill" /></div>
+            <div class="menuItem"><img class="icon" src="images/home.png" /><a type="submit" id="homeLink" href="home.php">Home</a></div>
+        </div>
 
-            </div>
+        <div id="tableWrapper">
+
+        </div>
     </body>
 </html>
