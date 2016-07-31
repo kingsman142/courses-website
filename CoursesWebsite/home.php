ed7a6f6771a7e955e18e6485a4130e6976217655
kingsman142 <kingsman142@yahoo.com>
2016-07-31 14:08:35 -0400

    moved homepage title into main div for better flow; switched main div from id=homecontent to class=main

--- CoursesWebsite/home.php
+++ CoursesWebsite/home.php
@@ -9,18 +9,21 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body src="Site.css">
-        <center>
-            <div class="title">
-                <div id="jobTitle">Job<div id="exploreTitle">Explore</div></div>
-            </div>
-        </center>
-
         <div id="menuWrapper">
             <div class="menuItem"><img class="icon" src="images/searchJob.png" /><input type="text" class="searchForm" onkeydown="if(enterKey(event)){ searchForJob() }" placeholder="Search by job" /></div>
             <div class="menuItem"><img class="icon" src="images/searchSkill.png" /><input type="text" class="searchForm" onkeydown="if(enterKey(event)){ searchForSkill() }" placeholder="Search by skill" /></div>
         </div>
 
-        <div id="homeContent">
+        <div class="main">
+            <center>
+                <div class="title">
+                    <div id="jobTitle">
+                        Job
+                        <div id="exploreTitle">Explore</div>
+                    </div>
+                </div>
+            </center>
+
             <div id="formsWrapper">
                 <input class="form job" placeholder="Occupation" />
                 <input class="form skill" placeholder="Skill" />
